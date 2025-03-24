a155929eca77edef499ca6dbec5538b40770bf39
sergiodiazpereira <sergiodiazpereira.guadalupe@alumnado.fundacionloyola.net>
2025-03-24 10:21:58 +0100

    Add files via upload

--- elegirLugar.php
+++ elegirLugar.php
@@ -1,37 +1,54 @@
 <?php
 //Conecta con la base de datos ($conexión)
+	//echo 'Inicio de sesion correcto, procede a elegir tu cuidad <a href="./elegirLugar.php">aquí</a>';
     include 'configdbH.php'; //include del archivo con los datos de conexión
 	$conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD); //Conecta con la base de datos
     $conexion->set_charset("utf8"); //Usa juego caracteres UTF8
 	//Desactiva errores
 	$controlador = new mysqli_driver();
     $controlador->report_mode = MYSQLI_REPORT_OFF;
+//Variables
+	$codigo=$_POST["codigo"];
+	$nombreJesuita=$_POST["njesuita"];
 //Cadena de caracteres de la consulta sql	
-	$sql="SELECT lugar FROM lugar;"; //Prepara la consulta
+	$sql="SELECT codigo, nombre FROM jesuita WHERE codigo = '".$codigo."' AND nombre = '".$nombreJesuita."';"; //Prepara la consulta
 	//echo $sql;	//Para probar
 	$resultado=$conexion->query($sql);	//Ejecuta la consulta sql
-?>
+//Existe el usuario??
+	if ($resultado->num_rows == 0) {
+		echo 'Inicio de sesión incorrecto, vuelve a iniciar sesión <a href="./index.html">aquí</a>';
+	} else {
+		session_start();
+		$fila=$resultado->fetch_array();
+		$_SESSION["njesuita"]=$fila["nombre"];
+				//Cadena de caracteres de la consulta sql de lugares	
+					$sql2="SELECT lugar FROM lugar;"; //Prepara la consulta
+					//echo $sql;	//Para probar
+					$resultado2=$conexion->query($sql2);	//Ejecuta la consulta sql
+				?>
 	
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Visita</title>
-    <link rel="stylesheet" href="./Camino.css">
-</head>
-<body>
-    <h1>Hola, <?php echo $_SESSION["njesuita"]?></h1>
-    <form method="post" action="./guardarVisita.php">
-		<input type="hidden" id="ip" name="ip" value="192.168.1.1">
-        <label for="ciudad">Selecciona una ciudad:</label>
-        <select id="ciudad" name="ciudad" required>
-		<?php
-			while($fila=$resultado->fetch_array()){
-				echo '<option class="visita" value="'.$fila["lugar"].'">'.$fila["lugar"].'</option>';
-			}
-		?>
-        </select>
-        <button type="submit">Registrar Visita</button>
-    </form>
-</body>
-</html>
+				<!DOCTYPE html>
+				<html>
+				<head>
+					<meta charset="UTF-8">
+					<title>Visita</title>
+					<link rel="stylesheet" href="./Camino.css">
+				</head>
+				<body>
+					<h1>Hola, <?php echo $_SESSION["njesuita"]?></h1>
+					<form method="post" action="./guardarVisita.php">
+						<label for="ciudad">Selecciona una ciudad:</label>
+						<select id="ciudad" name="ciudad" required>
+						<?php
+							while($fila=$resultado2->fetch_array()){
+								echo '<option class="visita" value="'.$fila["lugar"].'">'.$fila["lugar"].'</option>';
+							}
+						?>
+						</select>
+						<button type="submit">Registrar Visita</button>
+					</form>
+				</body>
+				</html>
+<?php
+	}
+?>
--- guardarVisita.php
+++ guardarVisita.php
@@ -1,4 +1,9 @@
 <?php
+//Recoge la información del formulario
+	session_start();
+	$nombreJesuita=$_SESSION["njesuita"];
+	$nombreCiudad=$_POST["ciudad"];
+	
 //Conecta con la base de datos ($conexión)
     include 'configdbH.php'; //include del archivo con los datos de conexión
 	$conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD); //Conecta con la base de datos
@@ -6,13 +11,26 @@
 	//Desactiva errores
 	$controlador = new mysqli_driver();
     $controlador->report_mode = MYSQLI_REPORT_OFF;
+	
 //Cadena de caracteres de la consulta sql	
-	$sql="INSERT INTO visita ('idJesuita','ip') VALUES ('".$_SESSION['njesuita']."','".$_POST['ip']."');"; //Prepara la consulta
-	//echo $sql;	//Para probar
-	$resultado=$conexion->query($sql);	//Ejecuta la consulta sql
-	if ($resultado === TRUE) {
-		echo "Visita guardada";
-	} else {
-		echo "Algo ha fallado";
-	}
+	$sqlCogerIdJesuita="SELECT idJesuita FROM jesuita WHERE nombre = '".$nombreJesuita."';";
+	$resultadoIdJesuita=$conexion->query($sqlCogerIdJesuita);
+	$filaIdJesuita = $resultadoIdJesuita->fetch_array();
+	$sqlCogerIpLugar="SELECT ip FROM lugar WHERE lugar = '".$nombreCiudad."';";
+	$resultadoIpLugar=$conexion->query($sqlCogerIpLugar);
+	$filaIpLugar = $resultadoIpLugar->fetch_array();
+	$sql="INSERT INTO visita (idJesuita, ip) VALUES ('".$filaIdJesuita['idJesuita']."','".$filaIpLugar['ip']."');";
+	
+	echo $sql; //envía el contenido de la variable al navegador, o sea, muestra la información en el navegador
+	
+//Ejecuta la consulta
+	$conexion->query($sql);
+	if($conexion->affected_rows>0)
+		echo "<h2>La visita ha sido guardada</h2>";
+	else{
+		echo '<h2>Algo ha fallado, <a href="./index.html">vuelve a intentarlo</a></h2>';
+	}	
+
+//Cierra la conexión
+	$conexion->close();
 ?>
