3acafc016939d49f9029ec78754080bc56fdb264
Sergio <sergiodiazpereira.guadalupe@alumnado.fundacionloyola.net>
2025-03-27 08:44:41 +0100

    Update configdbH.php

--- configdbH.php
+++ configdbH.php
@@ -1,6 +1,6 @@
 <?php
-define("SERVIDOR",'1daw.esvirgua.com'); 
-define("USUARIO",'user1daw_08'); 
-define("PASSWORD",'i!4j-=MA4jW2'); 
-define("BBDD",'user1daw_BD1-08'); 
+define("SERVIDOR",'localhost'); 
+define("USUARIO",'root'); 
+define("PASSWORD",''); 
+define("BBDD",'jesuitas'); 
 ?>
